5e796d9854962c4a8b0339e03b667ea56db47144
xiaozhuai <798047000@qq.com>
2022-05-18 00:26:42 +0800

    Fix macos opencv window size

--- opencv/src/main.cpp
+++ opencv/src/main.cpp
@@ -4,9 +4,9 @@
 int main() {
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
     auto image = cv::imread("assets/test.jpg");
+    cv::imshow("vcpkg_demo_opencv", image);
     cv::namedWindow("vcpkg_demo_opencv", cv::WINDOW_NORMAL);
     cv::resizeWindow("vcpkg_demo_opencv", 960, 540);
-    cv::imshow("vcpkg_demo_opencv", image);
     cv::waitKey(0);
     return 0;
 }
