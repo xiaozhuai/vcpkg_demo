26a020772a71be0a8e22350c628a64c954319e16
xiaozhuai <798047000@qq.com>
2022-11-25 12:23:35 +0800

    Optimize code

--- qt5_quick/src/AngleHelper.cpp
+++ qt5_quick/src/AngleHelper.cpp
@@ -2,18 +2,32 @@
 // Created by xiaozhuai on 2022/5/14.
 //
 
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+
 #include "AngleHelper.h"
 #include <QString>
 #include <QFile>
 #include <QDir>
+#include <Windows.h>
 
-#ifdef _WIN32
+static QString executablePath() {
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+    return QString::fromWCharArray(buffer);
+}
+
+static QString executableDirPath() {
+    QFileInfo exeFile(executablePath());
+    return exeFile.absoluteDir().path();
+}
 
 #define LIB_EGL "libEGL.dll"
 #define LIB_GLESv2 "libGLESv2.dll"
 
 bool isAngleAvailable() {
-    QDir exeDir(QCoreApplication::applicationDirPath());
+    QDir exeDir(executableDirPath());
     return QFile::exists(exeDir.filePath(LIB_EGL))
            && QFile::exists(exeDir.filePath(LIB_GLESv2));
 }
