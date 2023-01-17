bdb832239c0b23cf98c98da085ab50c422451749
xiaozhuai <798047000@qq.com>
2023-01-17 21:14:18 +0800

    Add imageinfo

---
+++ imageinfo/src/main.cpp
@@ -0,0 +1,16 @@
+#include <imageinfo.hpp>
+#include <iostream>
+
+int main() {
+    auto file = "assets/test.jpg";
+    auto imageInfo = getImageInfo<IIFilePathReader>(file);
+    std::cout << "File: " << file << "\n";
+    std::cout << "  - Error    : " << imageInfo.getErrorMsg() << "\n";
+    std::cout << "  - Width    : " << imageInfo.getWidth() << "\n";
+    std::cout << "  - Height   : " << imageInfo.getHeight() << "\n";
+    std::cout << "  - Format   : " << imageInfo.getFormat() << "\n";
+    std::cout << "  - Ext      : " << imageInfo.getExt() << "\n";
+    std::cout << "  - Full Ext : " << imageInfo.getFullExt() << "\n";
+    std::cout << "  - Mimetype : " << imageInfo.getMimetype() << "\n\n";
+    return 0;
+}
