3e5bee0bb5ede3ee515e2e47679b7519f79b9c9e
xiaozhuai <798047000@qq.com>
2023-01-17 13:45:09 +0800

    Add libyuv

---
+++ libyuv/src/main.cpp
@@ -0,0 +1,31 @@
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <cassert>
+#include <libyuv.h>
+#include <stb_image.h>
+
+#undef STB_IMAGE_IMPLEMENTATION
+
+int main() {
+    int width, height, channels;
+    auto *image = stbi_load("assets/test.jpg", &width, &height, &channels, 4);
+
+    assert(width % 2 == 0);
+    assert(height % 2 == 0);
+
+    auto *i420Image = new uint8_t[width * height * 3 / 2];
+    auto *i420Y = i420Image;
+    auto *i420U = i420Y + width * height;
+    auto *i420V = i420U + width * height / 4;
+
+    libyuv::RGBAToI420(
+            image, width * 4,
+            i420Y, width,
+            i420U, width / 2,
+            i420V, width / 2,
+            width, height);
+
+    stbi_image_free(image);
+    delete[] i420Image;
+    return 0;
+}
