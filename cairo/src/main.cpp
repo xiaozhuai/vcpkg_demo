ed00b3f89ad9086d486c5bf7c477e70b252a69b2
xiaozhuai <798047000@qq.com>
2023-02-02 19:52:00 +0800

    Optimize code

--- cairo/src/main.cpp
+++ cairo/src/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 #include <MiniFB_cpp.h>
-#include <cairo/cairo.h>
+#include <cairomm/cairomm.h>
 
 #ifdef _WIN32
 #define _USE_MATH_DEFINES
@@ -16,51 +16,54 @@
 
 #undef STB_IMAGE_IMPLEMENTATION
 
-void draw(cairo_t *ctx, int width, int height, cairo_surface_t *image) {
+using ContextPtr = Cairo::RefPtr<Cairo::Context>;
+using ImageSurfacePtr = Cairo::RefPtr<Cairo::ImageSurface>;
+
+void draw(const ContextPtr& ctx, int width, int height, const ImageSurfacePtr& image) {
     // clear
-    cairo_save(ctx);
-    cairo_set_source_rgba(ctx, 0.0, 0.0, 0.0, 0.0);
-    cairo_set_operator(ctx, CAIRO_OPERATOR_CLEAR);
-    cairo_paint(ctx);
-    cairo_restore(ctx);
+    ctx->save();
+    ctx->set_source_rgba(0.0, 0.0, 0.0, 0.0);
+    ctx->set_operator(Cairo::Context::Operator::CLEAR);
+    ctx->paint();
+    ctx->restore();
 
     // image
-    cairo_save(ctx);
-    double imageWidth = cairo_image_surface_get_width(image);
-    double imageHeight = cairo_image_surface_get_height(image);
-    cairo_scale(ctx, double(width) / imageWidth, double(height) / imageHeight);
-    cairo_set_source_surface(ctx, image, 0, 0);
-    cairo_paint(ctx);
-    cairo_restore(ctx);
+    ctx->save();
+    double imageWidth = image->get_width();
+    double imageHeight = image->get_height();
+    ctx->scale(double(width) / imageWidth, double(height) / imageHeight);
+    ctx->set_source(image, 0, 0);
+    ctx->paint();
+    ctx->restore();
 
     // star
-    cairo_save(ctx);
-    cairo_set_antialias(ctx, CAIRO_ANTIALIAS_BEST);
-    cairo_translate(ctx, 0.5f * double(width), 0.5f * double(height));
+    ctx->save();
+    ctx->set_antialias(Cairo::Antialias::ANTIALIAS_DEFAULT);
+    ctx->translate(0.5f * double(width), 0.5f * double(height));
     const float R = 0.45f * std::min(float(width), float(height));
     constexpr int N = 7;
     static_assert(N >= 5 && N % 2 == 1, "N must be odd and >=5");
     constexpr auto step = (N - 1) / 2;
     float theta = -M_PI_2;
-    cairo_move_to(ctx, R * cos(theta), R * sin(theta));
+    ctx->move_to(R * cos(theta), R * sin(theta));
     for (int i = 1; i < N; ++i) {
         theta += float(step) * M_PI * 2.0f / N;
-        cairo_line_to(ctx, R * cos(theta), R * sin(theta));
+        ctx->line_to(R * cos(theta), R * sin(theta));
     }
-    cairo_translate(ctx, -0.5f * double(width), -0.5f * double(height));
-    cairo_set_source_rgba(ctx, 1.0, 0.0, 0.0, 0.5);
-    cairo_fill_preserve(ctx);
-    cairo_restore(ctx);
+    ctx->translate(-0.5f * double(width), -0.5f * double(height));
+    ctx->set_source_rgba(1.0, 0.0, 0.0, 0.5);
+    ctx->fill_preserve();
+    ctx->restore();
 }
 
-cairo_surface_t *createSurface(int width, int height, uint8_t *framebuffer = nullptr) {
+ImageSurfacePtr createSurface(int width, int height, uint8_t *framebuffer = nullptr) {
     if (framebuffer == nullptr) {
-        return cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
+        return Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32, width, height);
     }
-    return cairo_image_surface_create_for_data(framebuffer, CAIRO_FORMAT_ARGB32, width, height, width * 4);
+    return Cairo::ImageSurface::create(framebuffer, Cairo::Surface::Format::ARGB32, width, height, width * 4);
 }
 
-cairo_surface_t *createSurfaceFromImageFile(const std::string &file) {
+ImageSurfacePtr createSurfaceFromImageFile(const std::string &file) {
     int width, height, channels;
     auto data = stbi_load(file.c_str(), &width, &height, &channels, 4);
     assert(width != 0 && height != 0 && data != nullptr);
@@ -84,16 +87,16 @@ int main() {
 
     std::vector<uint8_t> framebuffer(width * height * 4);
     auto surface = createSurface(width, height, framebuffer.data());
-    auto ctx = cairo_create(surface);
+    auto ctx = Cairo::Context::create(surface);
 
-    assert(width == cairo_image_surface_get_width(surface));
-    assert(height == cairo_image_surface_get_height(surface));
-    assert(width * 4 == cairo_image_surface_get_stride(surface));
+    assert(width == surface->get_width());
+    assert(height == surface->get_height());
+    assert(width * 4 == surface->get_stride());
 
     do {
         draw(ctx, width, height, image);
-        cairo_show_page(ctx);
-        int state = mfb_update_ex(window, cairo_image_surface_get_data(surface), width, height);
+        ctx->show_page();
+        int state = mfb_update_ex(window, surface->get_data(), width, height);
 
         if (state < 0) {
             mfb_close(window);
@@ -101,10 +104,7 @@ int main() {
         }
     } while (mfb_wait_sync(window));
 
-    stbi_image_free(cairo_image_surface_get_data(image));
-    cairo_surface_destroy(image);
-    cairo_surface_destroy(surface);
-    cairo_destroy(ctx);
+    stbi_image_free(image->get_data());
 
     return 0;
 }
