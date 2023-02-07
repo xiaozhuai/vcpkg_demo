441b505e89c82018a1d77358f96bd447d21d983f
xiaozhuai <798047000@qq.com>
2023-02-07 15:46:04 +0800

    Add xorstr

---
+++ xorstr/src/main.cpp
@@ -0,0 +1,9 @@
+#include <xorstr.hpp>
+#include <cstdio>
+
+int main() {
+    printf("%s\n", xorstr_("Hello World!"));
+    auto encrypted = xorstr("Hello World!");
+    printf("%s\n", encrypted.crypt_get());
+    return 0;
+}
