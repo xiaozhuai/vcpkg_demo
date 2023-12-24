388fe54cda6307f7260bcef66b5a59a9db449e5f
xiaozhuai <798047000@qq.com>
2023-12-24 18:33:16 +0800

    Update glfw and imgui

--- src/opengles_angle/src/main.cpp
+++ src/opengles_angle/src/main.cpp
@@ -7,7 +7,7 @@
 #include <cstdio>
 #include <string>
 
-#include "GLES2/gl2.h"
+#include "GLES3/gl3.h"
 #include "GLFW/glfw3.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -33,7 +33,7 @@ GLFWwindow *initWindow() {
     glfwSetErrorCallback(glfwErrorCallback);
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
