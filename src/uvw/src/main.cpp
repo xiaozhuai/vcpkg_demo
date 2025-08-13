86a30ca307692318ee64f434ebc340b05d22eee9
xiaozhuai <798047000@qq.com>
2025-08-13 15:36:58 +0800

    Optimize uvw demo code

--- src/uvw/src/main.cpp
+++ src/uvw/src/main.cpp
@@ -10,10 +10,20 @@
 class Discover {
 public:
     void start() {
-        loop_ = uvw::loop::get_default();
+        loop_ = uvw::loop::create();
         start_server();
         start_client();
+        start_signal();
         loop_->run();
+    }
+
+    void stop() {
+        stop_signal();
+        stop_client();
+        stop_server();
+        loop_->stop();
+        loop_->close();
+        loop_.reset();
     }
 
 private:
@@ -35,6 +45,15 @@ private:
         udp_client_->recv();
     }
 
+    void stop_client() {
+        timer_->stop();
+        timer_->close();
+        timer_->reset();
+        udp_client_->stop();
+        udp_client_->close();
+        udp_client_.reset();
+    }
+
     void start_server() {
         udp_server_ = loop_->resource<uvw::udp_handle>();
         udp_server_->bind(kBindAddress, kBroadcastPort);
@@ -48,6 +67,41 @@ private:
         udp_server_->recv();
     }
 
+    void stop_server() {
+        udp_server_->stop();
+        udp_server_->close();
+        udp_server_.reset();
+    }
+
+    void start_signal() {
+        sigint_handle_ = loop_->resource<uvw::signal_handle>();
+        sigint_handle_->on<uvw::signal_event>([&](const uvw::signal_event &event, uvw::signal_handle &) {
+            if (event.signum == SIGINT) {
+                printf("\rReceived SIGINT, stopping...\n");
+                stop();
+            }
+        });
+        sigint_handle_->start(SIGINT);
+
+        sigterm_handle_ = loop_->resource<uvw::signal_handle>();
+        sigterm_handle_->on<uvw::signal_event>([&](const uvw::signal_event &event, uvw::signal_handle &) {
+            if (event.signum == SIGTERM) {
+                printf("\rReceived SIGTERM, stopping...\n");
+                stop();
+            }
+        });
+        sigterm_handle_->start(SIGTERM);
+    }
+
+    void stop_signal() {
+        sigint_handle_->stop();
+        sigint_handle_->close();
+        sigint_handle_->reset();
+        sigterm_handle_->stop();
+        sigterm_handle_->close();
+        sigterm_handle_->reset();
+    }
+
 private:
     static constexpr uint16_t kBroadcastPort = 7550;
     static constexpr const char *kBroadcastAddress = "255.255.255.255";
@@ -56,6 +110,8 @@ private:
     std::shared_ptr<uvw::timer_handle> timer_;
     std::shared_ptr<uvw::udp_handle> udp_client_;
     std::shared_ptr<uvw::udp_handle> udp_server_;
+    std::shared_ptr<uvw::signal_handle> sigint_handle_;
+    std::shared_ptr<uvw::signal_handle> sigterm_handle_;
 };
 
 int main() {
