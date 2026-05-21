0bec34c2b4d14c0269f092ca6f15ac2a424927ca
xiaozhuai <798047000@qq.com>
2026-05-21 20:14:31 +0800

    更新libusb代码

--- src/libusb/src/main.cpp
+++ src/libusb/src/main.cpp
@@ -3,115 +3,184 @@
  */
 
 #include <algorithm>
+#include <chrono>
 #include <cstdio>
+#include <memory>
 #include <string>
 #include <thread>
 #include <vector>
 
 #include "libusb.h"
 
-std::vector<const char *> SPEED_ENUM_MAP = {
-    // clang-format off
-    "0 UNKNOWN",
-    "1 LOW (1.5Mbps) USB 1.0",
-    "2 FULL (12Mbps) USB 1.1",
-    "3 HIGH (480Mbps) USB 2.0",
-    "4 SUPER (5000Mbps) USB 3.0",
-    "5 SUPER+ (10000Mbps) USB 3.1",
-    // clang-format on
+const char *usb_version_get_speed(int speed) {
+    static const char *map[] = {
+        // clang-format off
+        "#0 UNKNOWN",
+        "#1 LOW (1.5Mbps)",
+        "#2 FULL (12Mbps)",
+        "#3 HIGH (480Mbps)",
+        "#4 SUPER (5000Mbps)",
+        "#5 SUPER+ 10000Mbps",
+        "#6 SUPER+ 20000Mbps",
+        // clang-format on
+    };
+    if (speed < 0 || speed >= std::size(map)) {
+        speed = 0;
+    }
+    return map[speed];
+}
+
+const char *usb_version_get_official_name(int speed) {
+    static const char *map[] = {
+        // clang-format off
+        "UNKNOWN",
+        "USB 1.0",
+        "USB 1.1",
+        "USB 2.0",
+        "USB 3.2 Gen 1",
+        "USB 3.2 Gen 2",
+        "USB 3.2 Gen 2x2"
+        // clang-format on
+    };
+    if (speed < 0 || speed >= std::size(map)) {
+        speed = 0;
+    }
+    return map[speed];
+}
+
+const char *usb_version_get_marketing_name(int speed) {
+    static const char *map[] = {
+        // clang-format off
+        "UNKNOWN",
+        "USB 1.0",
+        "USB 1.1",
+        "USB 2.0",
+        "USB 3.0",
+        "USB 3.1",
+        "USB 3.2",
+        // clang-format on
+    };
+    if (speed < 0 || speed >= std::size(map)) {
+        speed = 0;
+    }
+    return map[speed];
+}
+
+struct StringDescriptor {
+    int index;
+    std::string description;
 };
 
-struct DeviceDesc {
+struct DeviceDescription {
     uint8_t bus_number;
     uint8_t device_address;
     uint16_t vendor_id;
     uint16_t product_id;
     int speed;
-    std::vector<std::string> descriptors;
-
-    bool is_android() const {
-        return std::any_of(descriptors.begin(), descriptors.end(), [](const std::string &desc) {
-            return desc.find("ADB Interface") != std::string::npos || desc.find("Android") != std::string::npos;
-        });
-    }
-
-    bool is_apple() const {
-        return std::any_of(descriptors.begin(), descriptors.end(), [](const std::string &desc) {
-            return desc.find("Apple Mobile Device") != std::string::npos;
-        });
-    }
-
-    bool is_mobile() const { return is_android() || is_apple(); }
+    std::vector<StringDescriptor> string_descriptors;
 };
 
-std::vector<DeviceDesc> list_usb_devices() {
-    std::vector<DeviceDesc> device_list;
+std::vector<StringDescriptor> get_device_string_descriptors(libusb_device *device) {
+    libusb_device_handle *device_handle = nullptr;
+    int ret = libusb_open(device, &device_handle);
+    if (ret != LIBUSB_SUCCESS) {
+        // printf("libusb_open() failed: %s\n", libusb_error_name(ret));
+        return {};
+    }
+    std::unique_ptr<libusb_device_handle, decltype(&libusb_close)>  //
+        auto_close_device_handle(device_handle, libusb_close);
+    std::vector<StringDescriptor> string_descriptors;
+    if (device_handle) {
+        for (int desc_index = 1; desc_index < 256; desc_index++) {
+            unsigned char buffer[1024] = {0};
+            ret = libusb_get_string_descriptor_ascii(device_handle, desc_index, buffer, sizeof(buffer));
+            if (ret < 0) {
+                break;
+            }
+            string_descriptors.emplace_back(StringDescriptor{desc_index, reinterpret_cast<char *>(buffer)});
+        }
+    }
+    return string_descriptors;
+}
+
+DeviceDescription get_device_description(libusb_device *device) {
+    auto bus_number = libusb_get_bus_number(device);
+    auto device_address = libusb_get_device_address(device);
+    libusb_device_descriptor desc{};
+    libusb_get_device_descriptor(device, &desc);
+    auto vendor_id = desc.idVendor;
+    auto product_id = desc.idProduct;
+    auto speed = libusb_get_device_speed(device);
+    auto string_descriptors = get_device_string_descriptors(device);
+    return {bus_number, device_address, vendor_id, product_id, speed, string_descriptors};
+}
+
+void print_device_description(const DeviceDescription &device) {
+    const char *usb_version_speed = usb_version_get_speed(device.speed);
+    const char *usb_version_official_name = usb_version_get_official_name(device.speed);
+    const char *usb_version_marketing_name = usb_version_get_marketing_name(device.speed);
+    printf(
+        "- Device:\n"
+        "  - Bus: 0x%02X\n"
+        "  - Address: 0x%02X\n"
+        "  - VID: 0x%04X\n"
+        "  - PID: 0x%04X\n"
+        "  - USB Version:\n"
+        "    - Speed: %s\n"
+        "    - Official: %s\n"
+        "    - Marketing: %s\n",
+        device.bus_number, device.device_address, device.vendor_id, device.product_id,  //
+        usb_version_speed, usb_version_official_name, usb_version_marketing_name);
+    if (!device.string_descriptors.empty()) {
+        printf("  - String Descriptors:\n");
+        for (const auto &descriptor : device.string_descriptors) {
+            printf("    - [%d] %s\n", descriptor.index, descriptor.description.c_str());
+        }
+    }
+}
+
+void free_device_list(libusb_device **list) { libusb_free_device_list(list, 1); }
+
+std::vector<DeviceDescription> list_usb_devices() {
     libusb_context *context = nullptr;
-    libusb_init(&context);
-    libusb_device **devices;
+    int ret = libusb_init(&context);
+    if (ret != LIBUSB_SUCCESS) {
+        printf("libusb_init() failed: %s\n", libusb_error_name(ret));
+        return {};
+    }
+    std::unique_ptr<libusb_context, decltype(&libusb_exit)> auto_exit_context(context, libusb_exit);
+
+    libusb_device **devices = nullptr;
     ssize_t device_count = libusb_get_device_list(context, &devices);
+    if (device_count < 0) {
+        printf("libusb_get_device_list() failed\n");
+        return {};
+    }
+    std::unique_ptr<libusb_device *, decltype(&free_device_list)> auto_free_device_list(devices, free_device_list);
+
+    std::vector<DeviceDescription> device_list;
     for (int device_index = 0; device_index < device_count; device_index++) {
         libusb_device *device = devices[device_index];
-        uint8_t bus_number = libusb_get_bus_number(device);
-        uint8_t device_address = libusb_get_device_address(device);
-        libusb_device_descriptor desc{};
-        libusb_get_device_descriptor(device, &desc);
-        int speed = libusb_get_device_speed(device);
-
-        std::vector<std::string> descriptors;
-        libusb_device_handle *device_handle = nullptr;
-        libusb_open(device, &device_handle);
-        if (device_handle) {
-            for (int desc_index = 1; desc_index < 256; desc_index++) {
-                unsigned char buffer[1024] = {0};
-                int ret = libusb_get_string_descriptor_ascii(device_handle, desc_index, buffer, sizeof(buffer));
-                if (ret <= 0) {
-                    break;
-                }
-                descriptors.emplace_back((char *)buffer);
-            }
-            libusb_close(device_handle);
-        }
-
-        device_list.emplace_back(
-            DeviceDesc{bus_number, device_address, desc.idVendor, desc.idProduct, speed, descriptors});
+        device_list.emplace_back(get_device_description(device));
     }
-    libusb_free_device_list(devices, 1);
-    libusb_exit(context);
     return device_list;
 }
 
-void show_devices(const std::vector<DeviceDesc> &devices) {
-    for (int device_index = 0; device_index < devices.size(); device_index++) {
-        const auto &device = devices[device_index];
-        const char *speed_desc = SPEED_ENUM_MAP[device.speed];
-        printf(
-            "- Device[%d]\n"
-            "  - Bus: 0x%02X\n"
-            "  - Address: 0x%02X\n"
-            "  - VID: 0x%04X\n"
-            "  - PID: 0x%04X\n"
-            "  - Speed: %s\n"
-            "  - Mobile: %s\n",
-            device_index, device.bus_number, device.device_address, device.vendor_id, device.product_id, speed_desc,
-            !device.is_mobile()   ? "No"
-            : device.is_android() ? "Android"
-                                  : "Apple");
-        printf("  - Descriptors:\n");
-        for (int desc_index = 0; desc_index < device.descriptors.size(); desc_index++) {
-            printf("    - [%d] %s\n", desc_index, device.descriptors[desc_index].c_str());
-        }
-    }
-}
+void compare_devices(const std::vector<DeviceDescription> &devices_old,  //
+                     const std::vector<DeviceDescription> &devices_new,  //
+                     std::vector<DeviceDescription> &devices_plug,       //
+                     std::vector<DeviceDescription> &devices_unplug) {
+    auto compare_device = [](const DeviceDescription &a, const DeviceDescription &b) {
+        return a.vendor_id == b.vendor_id       //
+               && a.product_id == b.product_id  //
+               && a.bus_number == b.bus_number  //
+               && a.device_address == b.device_address;
+    };
 
-void compare_devices(const std::vector<DeviceDesc> &devices_old, const std::vector<DeviceDesc> &devices_new,
-                     std::vector<DeviceDesc> &devices_plug, std::vector<DeviceDesc> &devices_unplug) {
     for (const auto &device_new : devices_new) {
         bool found = false;
         for (const auto &device_old : devices_old) {
-            if (device_old.vendor_id == device_new.vendor_id && device_old.product_id == device_new.product_id &&
-                device_old.bus_number == device_new.bus_number &&
-                device_old.device_address == device_new.device_address) {
+            if (compare_device(device_old, device_new)) {
                 found = true;
                 break;
             }
@@ -123,9 +192,7 @@ void compare_devices(const std::vector<DeviceDesc> &devices_old, const std::vect
     for (const auto &device_old : devices_old) {
         bool found = false;
         for (const auto &device_new : devices_new) {
-            if (device_old.vendor_id == device_new.vendor_id && device_old.product_id == device_new.product_id &&
-                device_old.bus_number == device_new.bus_number &&
-                device_old.device_address == device_new.device_address) {
+            if (compare_device(device_old, device_new)) {
                 found = true;
                 break;
             }
@@ -140,26 +207,33 @@ int main() {
     auto devices = list_usb_devices();
     printf("----------------------------------\n");
     printf("All Devices:\n");
-    show_devices(devices);
+    for (const auto &device : devices) {
+        print_device_description(device);
+    }
 
     for (;;) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         auto devices_new = list_usb_devices();
-        std::vector<DeviceDesc> devices_plug;
-        std::vector<DeviceDesc> devices_unplug;
+        std::vector<DeviceDescription> devices_plug;
+        std::vector<DeviceDescription> devices_unplug;
         compare_devices(devices, devices_new, devices_plug, devices_unplug);
         devices_new.swap(devices);
         if (!devices_plug.empty()) {
             printf("----------------------------------\n");
             printf("Devices Plug:\n");
-            show_devices(devices_plug);
+            for (const auto &device : devices_plug) {
+                print_device_description(device);
+            }
         }
         if (!devices_unplug.empty()) {
             printf("----------------------------------\n");
             printf("Devices Unplug:\n");
-            show_devices(devices_unplug);
+            for (const auto &device : devices_unplug) {
+                print_device_description(device);
+            }
         }
     }
 
+    printf("\nSIGINT received, exit.\n");
     return 0;
 }
